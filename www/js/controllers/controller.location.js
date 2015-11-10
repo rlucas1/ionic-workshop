1f2e1567b505da50e4af4ebee9bf5850256a9fdf
rlucas <r.lucas@code-troopers.com>
2015-11-10 17:32:43 +0100

    my location works on android

--- www/js/controllers/controller.location.js
+++ www/js/controllers/controller.location.js
@@ -2,47 +2,61 @@ angular.module('controllers')
     .controller('locationCtrl', function ($scope,uiGmapGoogleMapApi,$cordovaGeolocation) {
 
         $scope.map = { center: { latitude: 47.390031, longitude: 0.688913 }, zoom: 10 };
-        $scope.markers = [];
+        var myPosition;
+        $scope.markers = [myPosition];
 
         uiGmapGoogleMapApi.then(function(maps) {
 
-
-            $cordovaGeolocation.getCurrentPosition().then(function(pos) {
-                console.log("WONDELLe : "+JSON.stringify(pos));
-                addMarker("Current position",47.390031,0.688913 );
+            ionic.Platform.ready(function () {
+                onDeviceReady();
             });
 
-            addMarker("Current position", 47.390031, 0.688913);
-            var watchOptions = {
-                frequency : 500,
-                timeout : 1000,
-                enableHighAccuracy: true // may cause errors if true
-            };
-
-            var watch = $cordovaGeolocation.watchPosition(watchOptions);
-            watch.then(
-                null,
-                function(err) {
-                    console.log("ouch");
-                },
-                function(position) {
-                    var lat  = position.coords.latitude
-                    var long = position.coords.longitud
-                    console.log("WONDELLe 2 : "+JSON.stringify(position));
+            function onDeviceReady() {
+                $cordovaGeolocation.getCurrentPosition().then(function (pos) {
+                    myPosition = {
+                        id: "Current position",
+                        latitude: pos.coords.latitude,
+                        longitude: pos.coords.longitude,
+                        title: "Current position"
+                    };
+                    addMarker(myPosition);
                 });
 
+                //addMarker("Current position", 47.390031, 0.688913);
+                var watchOptions = {
+                    frequency: 1000,
+                    timeout: 5000,
+                    enableHighAccuracy: true // may cause errors if true
+                };
 
-            function addMarker(name,lat,lng){
-                var latitude = lat;
-                var longitude = lng;
-                $scope.markers.push({
-                    id:name,
-                    latitude: latitude,
-                    longitude: longitude,
-                    title: name
-                });
+                var watch = $cordovaGeolocation.watchPosition(watchOptions);
+                watch.then(
+                    null,
+                    function (err) {
+                        alert("ouch");
+                    },
+                    function (position) {
+                        updatePosition(position.coords.latitude, position.coords.longitude);
+                    });
+
+
+                function updatePosition(lat, lng) {
+                    myPosition.latitude = lat;
+                    myPosition.longitude = lng;
+                }
+
+                function addMarker(position) {
+                    $scope.markers.push({
+                        id: position.id,
+                        latitude: position.latitude,
+                        longitude: position.longitude,
+                        title: position.name
+                    });
+
+                }
+
+
             }
-
         });
 
 
