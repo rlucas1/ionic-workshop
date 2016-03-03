112f30313d74e345471ee034fc367e763a849b66
rlucas <r.lucas@code-troopers.com>
2016-03-03 11:51:17 +0100

    adding script to install cordova plugin

---
+++ installPlugin.sh
@@ -0,0 +1,5 @@
+
+cordova plugin add cordova-plugin-device
+cordova plugin add cordova-plugin-geolocation
+cordova plugin add cordova-plugin-whitelist
+cordova plugin add https://github.com/katzer/cordova-plugin-local-notifications.git
